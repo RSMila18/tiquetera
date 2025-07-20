823d221f5a6ac2a3aae5fe34f57c1fbf2f4d3ae3
Camila Rios <mriosm@unal.edu.co>
2025-07-19 20:36:00 -0500

    .

--- includes/header.php
+++ includes/header.php
@@ -7,11 +7,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Trabajo final BD1</title>
 
-    <link href="http://localhost/tiquetera/" rel="stylesheet"
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 </head>
 
 <body>
     <div class="container mt-5">
 
-        <a href="/">&#8592; Volver al inicio</a>
+        <a href="tiquetera/index.php">&#8592; Volver al inicio</a>
